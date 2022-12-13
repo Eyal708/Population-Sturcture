7542d466a410c2a2960d4db13a067d2342d03621
Eyal708 <eyal7090@gmail.com>
2022-12-13 15:24:08 +0200

    check changes after setting virtual enviroment

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ from Migration import Migration
 import numpy as np
 
 M_test = np.array([[0, 0.3, 0.2], [0.1, 0, 0.4], [0.4, 0.2, 0]])
-migration_mat = Migration(M_test) #some comment
+migration_mat = Migration(M_test)  # some comment
+# comment meant to check if fit is set up properly
 A_matrix = migration_mat.produce_coefficient_matrix()  # coefficient matrix
 A_rank = np.linalg.matrix_rank(A_matrix)  # coefficient matrix rank
 print("Migration matrix is:\n", M_test, "\n")
